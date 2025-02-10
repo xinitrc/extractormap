e5578f1dc62d2ed405983ad3965890262f71c227
Martin Hilscher <mh@xinitrc.de>
2025-02-10 13:57:22 +0100

    Added tsup library generation

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    clean: true,
+    dts: true,
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm', 'iife'],
+    minify: 'terser',
+    outDir: 'lib',
+    platform: 'node',
+    splitting: true,
+    sourcemap: false,
+});
+
