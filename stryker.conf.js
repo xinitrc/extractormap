9ce822043c6da830e9eb8995b3695d76df957fc3
Martin Hilscher <mh@xinitrc.de>
2025-02-11 22:45:51 +0100

    Added reporter back in

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,7 +4,7 @@ module.exports =  {
     checkers: ['typescript'],
     coverageAnalysis: "perTest",
     packageManager: "npm",
-    reporters: ["html", "progress", "json"],
+    reporters: ["html", "clear-text", "progress", "json"],
     testRunner: "jest",
     tsconfigFile: "tsconfig.json",
     mutate: ["src/**/*.ts"],
