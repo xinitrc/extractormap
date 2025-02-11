f58aee3d77011697df147077862a1cc23134c42b
Martin Hilscher <mh@xinitrc.de>
2025-02-11 19:52:39 +0100

    Simplified and reorganized

--- src/ExtractorMap.ts
+++ src/ExtractorMap.ts
@@ -1,5 +1,6 @@
 import {jpv} from './extractorFunctions/jsonPathExtractor';
 
+/* Types */
 type __PathImplementation<T, Key extends keyof T> =
     Key extends string
         ? T[Key] extends Record<string, any>
@@ -44,109 +45,119 @@ export type ExtractorMap<T, S extends Record<string, any> = any> = {
 
 export type DeepPartial<T> = T extends Record<string, any> ? { [K in keyof T]?: DeepPartial<T[K]> } : T;
 
-export function extract<T, S extends Record<string, any> = any>(te: TypeExtractor<T, S>, input: S): T;
-export function extract<T, S extends Record<string, any> = any>(te: TypeExtractor<T, S>): ExtractorFunction<T, S>;
-export function extract<T, S extends Record<string, any>>(te: TypeExtractor<T, S>, input?: S): T | ExtractorFunction<T, S> {
-    const extractorFunction: ExtractorFunction<T, S> = isPath(te) ? jpv<T, S>(te) : (isExtractorFunction<T, S>(te) ? te : extractorFunctionFromExtractorMap<T, S>(te));
-
-    return typeof input === 'undefined' ? extractorFunction : extractorFunction(input);
-}
-
-export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, valuesInterpretedAsEmpty?: any[]): ExtractorFunction<DeepPartial<T>, S>;
-export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, valuesInterpretedAsEmpty?: any[] | S, input?: S): DeepPartial<T>;
-export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, first?: any[] | S, second?: S): DeepPartial<T> | ExtractorFunction<DeepPartial<T>, S> {
-    const valuesInterpretedAsEmpty = Array.isArray(first) ? first : [];
-    const input: S = Array.isArray(first) ? second : first;
-
-    const extractorFunction: ExtractorFunction<DeepPartial<T>, S> = (inputObject: S): DeepPartial<T> => extractFromObjectFilteringEmpties(inputObject, valuesInterpretedAsEmpty, map);
-
-    return typeof input === 'undefined' ? extractorFunction : extractorFunction(input);
-}
-
-function isExtractorFunction<T, S extends Record<string, any>>(e: any): e is ExtractorFunction<T, S> {
+/* Type Guards */
+function _isExtractorFunction<T, S extends Record<string, any>>(e: any): e is ExtractorFunction<T, S> {
     return typeof e === 'function';
 }
 
-function isPath(e: any): e is string {
+function _isPath(e: any): e is string {
     return typeof e === 'string';
 }
 
-function extractorFunctionFromExtractorMap<T, S extends Record<string, any>>(extractionMap: ExtractorMap<T, S>): ExtractorFunction<T, S> {
+function _isKeyOfT<T, S extends Record<string, any>>(eMap: ExtractorMap<T, S>, keyUnderTest: string | number | symbol): keyUnderTest is keyof T {
+    return typeof eMap[keyUnderTest] !== 'undefined';
+}
+
+/* Helper Functions */
+function _convertExtractorMapToExtractorFunctionFilteringEmpties<T, S extends Record<string, any>>(extractionMap: ExtractorMap<T, S>, valuesInterpretedAsEmpty: unknown[]): ExtractorFunction<DeepPartial<T>, S> {
+    return (input: S): DeepPartial<T> => {
+        const resultObject: DeepPartial<T> = {} as DeepPartial<T>;
+        const extractionKeys: Array<keyof T> = Object.keys(extractionMap) as Array<keyof T>;
+
+        extractionKeys.forEach((extractionKey: keyof T): void => {
+            const extractor: ExtractorFunction<DeepPartial<T[typeof extractionKey]>, S> = _convertTypeExtractorToExtractorFunctionFilteringEmpties<T[typeof extractionKey], S>(extractionMap[extractionKey], valuesInterpretedAsEmpty);
+
+            const value: DeepPartial<T[typeof extractionKey]> = extractor(input);
+
+            // To some advanced equivalency magic in javascript this is not equivalent to
+            // (valuesInterpretedAsEmpty.indexOf(value) < 0)
+            // Trust me you don't want me to explain
+            const containedInExcludes = valuesInterpretedAsEmpty.reduce((a: boolean, b: any) => (!!a || b === value), false);
+
+            if (!containedInExcludes) {
+                resultObject[extractionKey] = value as any; // screw this
+            }
+        });
+
+        return resultObject;
+    };
+}
+
+function _convertTypeExtractorToExtractorFunctionFilteringEmpties<T, S extends Record<string, any> = any>(typeExtractor: TypeExtractor<T, S>, valuesInterpretedAsEmpty: unknown[]): ExtractorFunction<DeepPartial<T>, S> {
+    if (_isPath(typeExtractor)) {
+        return jpv(typeExtractor);
+    } else if (_isExtractorFunction<T, S>(typeExtractor)) {
+        return typeExtractor as ExtractorFunction<DeepPartial<T>, S>;
+    } else {
+        return _convertExtractorMapToExtractorFunctionFilteringEmpties<T, S>(typeExtractor, valuesInterpretedAsEmpty);
+    }
+}
+
+function _convertExtractorMapToExtractorFunction<T, S extends Record<string, any>>(extractionMap: ExtractorMap<T, S>): ExtractorFunction<T, S> {
     return (input: S): T => {
         const resultObject: Partial<T> = {};
         const extractionKeys: Array<keyof T> = Object.keys(extractionMap) as Array<keyof T>;
 
         extractionKeys.forEach((extractionKey: keyof T): void => {
-            resultObject[extractionKey] = _extractForKey(extractionMap, extractionKey, input) as any; // typescript compiler is unable to determine type
+            const extractor: ExtractorFunction<T[typeof extractionKey], S> = extract<T[typeof extractionKey], S>(extractionMap[extractionKey]);
+
+            resultObject[extractionKey] = extractor(input);
         });
 
         return resultObject as T;
     };
 }
 
-function extractFromObjectFilteringEmpties<T, S extends Record<string, any>>(inputObject: S, valuesInterpretedAsEmpty: unknown[], extractionMap: ExtractorMap<T, S>): DeepPartial<T> {
-    const resultObject: DeepPartial<T> = {} as DeepPartial<T>;
-    const extractionKeys: Array<keyof T> = Object.keys(extractionMap) as Array<keyof T>;
-
-    extractionKeys.forEach((extractionKey: keyof T): void => {
-        const value = _extractForKeyPartial(valuesInterpretedAsEmpty, extractionMap, extractionKey, inputObject);
-
-        // To some advanced equivalency magic in javascript this is not equivalent to
-        // (valuesInterpretedAsEmpty.indexOf(value) < 0)
-        // Trust me you don't want me to explain
-        const containedInExcludes = valuesInterpretedAsEmpty.reduce((a: boolean, b: any) => (!!a || b === value), false);
-
-        if (!containedInExcludes) {
-            resultObject[extractionKey] = value as any; // screw this
-        }
-    });
-
-    return resultObject;
+function _convertTypeExtractorToExtractorFunction<T, S extends Record<string, any> = any>(typeExtractor: TypeExtractor<T, S>): ExtractorFunction<T, S> {
+    if (_isPath(typeExtractor)) {
+        return jpv<T, S>(typeExtractor);
+    } else if (_isExtractorFunction<T, S>(typeExtractor)) {
+        return typeExtractor;
+    } else {
+        return _convertExtractorMapToExtractorFunction<T, S>(typeExtractor);
+    }
 }
 
-export function createExtractingProxy<T, S extends Record<string, any> = any>(eMap: ExtractorMap<T, S>, input: S): T;
-export function createExtractingProxy<T, S extends Record<string, any> = any>(eMap: ExtractorMap<T, S>): ExtractorFunction<T, S>;
-export function createExtractingProxy<T, S extends Record<string, any> = any>(eMap: ExtractorMap<T, S>, input?: S): T | ExtractorFunction<T, S> {
-    if (typeof input === 'undefined') {
-        return (inputObject: S): T => createExtractingProxy(eMap, inputObject);
-    } else {
+function _proxyFactory<T, S extends Record<string, any> = any>(eMap: ExtractorMap<T, S>): ExtractorFunction<T, S> {
+    return (input: S) => {
         return new Proxy({}, {
             get(_: any, name: PropertyKey): any {
                 if (_isKeyOfT(eMap, name)) {
-                    return _extractForKey(eMap, name, input);
+                    return extract<T[typeof name], S>(eMap[name], input);
                 } else {
                     throw new ReferenceError(`Property "${name.toString()}" does not exist.`);
                 }
             }
         }) as unknown as T;
-    }
+    };
 }
 
-function _extractForKey<T, S extends Record<string, any>>(extractionMap: ExtractorMap<T, S>, extractionKey: keyof T, inputObject: S): T[typeof extractionKey] {
-    const extractor: ExtractorFunction<T[typeof extractionKey], S> = extract(extractionMap[extractionKey]);
-
-    return extractor(inputObject);
+function _extractorFactory<T, S extends Record<string, any> = any>(extractorFunction: ExtractorFunction<T, S>, input: S): T;
+function _extractorFactory<T, S extends Record<string, any> = any>(extractorFunction: ExtractorFunction<T, S>): ExtractorFunction<T, S>;
+function _extractorFactory<T, S extends Record<string, any> = any>(extractorFunction: ExtractorFunction<T, S>, input?: S): T | ExtractorFunction<T, S> {
+    return typeof input === 'undefined' ? extractorFunction : extractorFunction(input);
 }
 
-function _extractForKeyPartial<T, S extends Record<string, any>>(valuesInterpretedAsEmpty: unknown[], extractionMap: ExtractorMap<T, S>, extractionKey: keyof T, inputObject: S): DeepPartial<T[keyof T]> {
-    const extractorCandidate: TypeExtractor<T[typeof extractionKey], S> = extractionMap[extractionKey];
+/** Exported module functionality */
+export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, valuesInterpretedAsEmpty?: unknown[]): ExtractorFunction<DeepPartial<T>, S>;
+export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, valuesInterpretedAsEmpty?: unknown[] | S, input?: S): DeepPartial<T>;
+export function extractFilteringEmpties<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, first?: unknown[] | S, second?: S): DeepPartial<T> | ExtractorFunction<DeepPartial<T>, S> {
+    const valuesInterpretedAsEmpty: unknown[] = Array.isArray(first) ? first : [];
+    const input: S = Array.isArray(first) ? second : first;
 
-    const generateExtractor: (input: TypeExtractor<T[typeof extractionKey], S>) => ExtractorFunction<DeepPartial<T[typeof extractionKey]>, S> =
-        (input: TypeExtractor<T[typeof extractionKey], S>): ExtractorFunction<DeepPartial<T[typeof extractionKey]>, S> => {
-            if (isPath(input)) {
-                return jpv(input);
-            } else if (isExtractorFunction(input)) {
-                return input as ExtractorFunction<DeepPartial<T[typeof extractionKey]>, S>;
-            } else {
-                return extractFilteringEmpties(input as ExtractorMap<T[typeof extractionKey], S>, valuesInterpretedAsEmpty);
-            }
-        };
+    const extractorFunction: ExtractorFunction<DeepPartial<T>, S> = _convertExtractorMapToExtractorFunctionFilteringEmpties(map, valuesInterpretedAsEmpty);
 
-    const extractor: ExtractorFunction<DeepPartial<T[typeof extractionKey]>, S> = generateExtractor(extractorCandidate);
-
-    return extractor(inputObject);
+    return _extractorFactory(extractorFunction, input);
 }
 
-function _isKeyOfT<T>(eMap: ExtractorMap<T>, keyUnderTest: string | number | symbol): keyUnderTest is keyof T {
-    return typeof eMap[keyUnderTest] !== 'undefined';
+export function extract<T, S extends Record<string, any> = any>(typeExtractor: TypeExtractor<T, S>, input: S): T;
+export function extract<T, S extends Record<string, any> = any>(typeExtractor: TypeExtractor<T, S>): ExtractorFunction<T, S>;
+export function extract<T, S extends Record<string, any>>(typeExtractor: TypeExtractor<T, S>, input?: S): T | ExtractorFunction<T, S> {
+    return _extractorFactory(_convertTypeExtractorToExtractorFunction<T, S>(typeExtractor), input);
+}
+
+export function createExtractingProxy<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, input: S): T;
+export function createExtractingProxy<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>): ExtractorFunction<T, S>;
+export function createExtractingProxy<T, S extends Record<string, any> = any>(map: ExtractorMap<T, S>, input?: S): T | ExtractorFunction<T, S> {
+    return _extractorFactory(_proxyFactory(map), input);
 }
