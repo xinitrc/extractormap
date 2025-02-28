d5f6e7203b1be983c5b9eb1f6d79f8f89a06b6b5
Martin Hilscher <mh@xinitrc.de>
2025-02-28 08:19:18 +0100

    refactor: improved typing and used fp to simplify jsonpath generators

--- src/extractorFunctions/jsonPathExtractor.ts
+++ src/extractorFunctions/jsonPathExtractor.ts
@@ -1,29 +1,27 @@
 import {JSONPath} from 'jsonpath-plus';
-import {ExtractorFunction, Path_Type, ConverterFunction, identity} from '..';
+import {ExtractorFunction, Path_Type, ConverterFunction, identity, fmap, compose} from '..';
 
-export function jpa<T>(jsonPath: string, convert: ConverterFunction<T> = identity): ExtractorFunction<T, Record<string, any>> {
-    return (inputObject: Record<string, any>): T => {
-        return convert(JSONPath({json: inputObject, path: jsonPath}));
-    };
+function jsonPathExtraction(jsonPath: string): ExtractorFunction<any[], Record<string, any>> {
+    return (input: Record<string, any>): any => JSONPath({json: input, path: jsonPath});
+}
+
+function jsonPathExtractionFirst<T, S extends Record<string, any>>(jsonPath: string): ExtractorFunction<T, S> {
+    const first = (input: any[]): any => input[0];
+    const asT = (input: any): T => input as T;
+
+    return compose(asT, first, jsonPathExtraction(jsonPath));
+}
+
+export function jpa<T>(jsonPath: string, convert: ConverterFunction<T, any[]> = identity): ExtractorFunction<T, Record<string, any>> {
+    return compose(convert, jsonPathExtraction(jsonPath))
 }
 
 export function jpq<T>(jsonPath: string, convert: ConverterFunction<T> = identity): ExtractorFunction<Array<T>, Record<string, any>> {
-    return (inputObject: Record<string, any>): Array<T> => {
-        const vals: Array<any> = JSONPath({json: inputObject, path: jsonPath});
-        return vals.map(convert);
-    };
+    return compose(fmap(convert), jsonPathExtraction(jsonPath));
 }
 
-export function jpv<T, S extends Record<string, any> = any, I = T>(jsonPath: Path_Type<I, S>, _convert?: ConverterFunction<T, I>): ExtractorFunction<T, S> {
-    if (typeof _convert === 'undefined') {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        return (inputObject: S): T => JSONPath({json: inputObject, path: jsonPath})[0] as T;
-    } else {
-        return (inputObject: S): T => {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            const intermediateResult: I = JSONPath({json: inputObject, path: jsonPath})[0] as I;
-
-            return _convert(intermediateResult);
-        };
-    }
+export function jpv<T, S extends Record<string, any> = any>(jsonPath: Path_Type<T, S>): ExtractorFunction<T, S>
+export function jpv<T, I, S extends Record<string, any> = any>(jsonPath: Path_Type<I, S>, _convert: ConverterFunction<T, I>): ExtractorFunction<T, S>
+export function jpv<T, I, S extends Record<string, any> = any>(jsonPath: string, _convert?: ConverterFunction<T, I>): unknown {
+    return typeof _convert === 'undefined' ? jsonPathExtractionFirst(jsonPath) : compose(_convert, jsonPathExtractionFirst<I, S>(jsonPath));
 }
--- src/helper/functionalHelper.ts
+++ src/helper/functionalHelper.ts
@@ -1,8 +1,9 @@
-// General Helper
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 import {ConverterFunction} from '../ExtractorMap.types';
 
+// Typehelper
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 type _EQ<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<TT>() => TT extends Y ? 1 : 2) ? true : false;
+type _AND<X extends boolean, Y extends boolean> = X extends true ? Y : false;
 
 // List Helper
 type _Tail<T extends readonly unknown[]> = T extends readonly [any, ...infer RST] ? RST : [];
@@ -13,18 +14,25 @@ type _ConditionalFirstParameter<T> = T extends (x: infer P) => unknown ? P : nev
 // Check if array is composable types
 type _IsComposable<OutputT, FN> = FN extends (x: any) => OutputT ? true : false;
 type _AreComposable<OutputT, InputT, FNs extends readonly unknown[]> = FNs['length'] extends 0 ? _EQ<OutputT, InputT>
-    : (_IsComposable<OutputT, FNs[0]> extends true ? _AreComposable<_ConditionalFirstParameter<FNs[0]>, InputT, _Tail<FNs>> : false);
+    : _AND<_IsComposable<OutputT, FNs[0]>, _AreComposable<_ConditionalFirstParameter<FNs[0]>, InputT, _Tail<FNs>>>;
 type _Composables<OutputT, InputT, FNs extends readonly unknown[]> = _AreComposable<OutputT, InputT, FNs> extends true ? FNs & ReadonlyArray<UnaryFunction<any, any>> : never;
 
 type UnaryFunction<O, I> = (input: I) => O;
-export type ComposeInput<Z, Y, B, A, K extends readonly unknown[]> = [UnaryFunction<Z, B>, UnaryFunction<B, A>] | [UnaryFunction<Z, Y>, ..._Composables<Y, B, K>, UnaryFunction<B, A>];
+export type ComposeInput<Z, Y, B, A, K extends readonly unknown[]> = [UnaryFunction<Z, Y>, ..._Composables<Y, B, K>, UnaryFunction<B, A>];
 
-export function compose<Z, Y, B, A, K extends readonly unknown[]>(...args: ComposeInput<Z, Y, B, A, K>): UnaryFunction<Z, A> {
+/** This function implements general functional composition
+ * it combines the functions from right to left (as is usual in mathematics)
+ * compose(f, g, h) is equivalent to (input) => f(g(h(input)))
+ * the input and output type will make sure that the types of the functions
+ * are compatible with one another
+ **/
+export function compose<Z, Y, A>(...args: [UnaryFunction<Z, Y>, UnaryFunction<Y, A>]): UnaryFunction<Z, A>
+export function compose<Z, Y, A, B, K extends readonly unknown[]>(...args: ComposeInput<Z, Y, B, A, K>): UnaryFunction<Z, A>
+export function compose<Z, A>(...args: ReadonlyArray<UnaryFunction<any, any>>): UnaryFunction<Z, A> {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return (input: A): Z => args.reverse().reduce((a: any, fn: UnaryFunction<any, any>): any => fn(a), input);
+    return (input: A): Z => args.reduceRight((a: any, fn: UnaryFunction<any, any>): any => fn(a), input);
 }
 
 export function fmap<T, S>(fn: ConverterFunction<T, S>): ConverterFunction<Array<T>, Array<S>> {
     return (input: Array<S>): Array<T> => input.map(fn);
 }
-
