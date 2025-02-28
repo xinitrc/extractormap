1c3c284cf91fdf2462869e4992d9aec3e956c4f9
Martin Hilscher <mh@xinitrc.de>
2025-02-28 08:20:45 +0100

    refactor: improved typing

--- src/converterFunctions/baseConverter.ts
+++ src/converterFunctions/baseConverter.ts
@@ -1,7 +1,7 @@
 import {ConverterFunction} from '..';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-function _answerWithDefault<T>(inputValue: unknown, forVals: Array<unknown>): inputValue is T {
+function _answerWithDefault<T, Unknonws extends ReadonlyArray<unknown>>(inputValue: unknown, forVals: Unknonws): inputValue is T {
     return forVals.filter((forVal: unknown): boolean => inputValue === forVal).length > 0;
 }
 
@@ -10,10 +10,20 @@ export function identity<T, I>(x: I): T {
     return x as unknown as T;
 }
 
-export function defaultTo<T>(defaultValue: T, forVals: Array<unknown> = [undefined]): ConverterFunction<T, unknown> {
-    return (inputValue: unknown): T => {
-        const answerWithDefault = _answerWithDefault(inputValue, forVals);
+export function defaultTo<T>(defaultValue: T): ConverterFunction<T, T | undefined>
+export function defaultTo<T, UKNS extends readonly unknown[]>(defaultValue: T, forVals: UKNS): ConverterFunction<T, T | UKNS[number]>
+export function defaultTo<T, UKNS extends readonly unknown[]>(defaultValue: T, forVals?: UKNS): unknown {
+    if (typeof forVals === 'undefined') {
+        return (inputValue: T | undefined): T => {
+            const answerWithDefault = _answerWithDefault(inputValue, [undefined]);
 
-        return answerWithDefault ? defaultValue : inputValue;
-    };
+            return answerWithDefault ? defaultValue : inputValue;
+        }
+    } else {
+        return (inputValue: T | UKNS[number]): T => {
+            const answerWithDefault = _answerWithDefault(inputValue, forVals);
+
+            return answerWithDefault ? defaultValue : inputValue;
+        };
+    }
 }
