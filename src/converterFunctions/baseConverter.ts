61203120dea58da524c305ee415b0fbe80e49aee
Martin Hilscher <mh@xinitrc.de>
2025-03-10 14:21:16 +0100

    refactor: simplyfied defaultTo function

--- src/converterFunctions/baseConverter.ts
+++ src/converterFunctions/baseConverter.ts
@@ -1,8 +1,8 @@
 import {ConverterFunction} from '..';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-function _answerWithDefault<T, Unknonws extends ReadonlyArray<unknown>>(inputValue: unknown, forVals: Unknonws): inputValue is T {
-    return forVals.filter((forVal: unknown): boolean => inputValue === forVal).length > 0;
+function _answerWithInput<T, Unknonws extends readonly unknown[]>(inputValue: unknown, forVals: Unknonws): inputValue is T {
+    return forVals.filter((forVal: unknown): boolean => inputValue === forVal).length <= 0;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
@@ -10,20 +10,12 @@ export function identity<T, I>(x: I): T {
     return x as unknown as T;
 }
 
+function _defaultTo<T, UKNS extends readonly unknown[]>(defaultValue: T, forVals: UKNS): ConverterFunction<T, T | UKNS[number]> {
+    return (inputValue: T | UKNS[number]): T => _answerWithInput<T, UKNS>(inputValue, forVals) ? inputValue : defaultValue;
+}
+
 export function defaultTo<T>(defaultValue: T): ConverterFunction<T, T | undefined>
 export function defaultTo<T, UKNS extends readonly unknown[]>(defaultValue: T, forVals: UKNS): ConverterFunction<T, T | UKNS[number]>
 export function defaultTo<T, UKNS extends readonly unknown[]>(defaultValue: T, forVals?: UKNS): unknown {
-    if (typeof forVals === 'undefined') {
-        return (inputValue: T | undefined): T => {
-            const answerWithDefault = _answerWithDefault(inputValue, [undefined]);
-
-            return answerWithDefault ? defaultValue : inputValue;
-        }
-    } else {
-        return (inputValue: T | UKNS[number]): T => {
-            const answerWithDefault = _answerWithDefault(inputValue, forVals);
-
-            return answerWithDefault ? defaultValue : inputValue;
-        };
-    }
+    return _defaultTo(defaultValue, forVals ?? [undefined]);
 }
