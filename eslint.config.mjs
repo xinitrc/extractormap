d313d02aeee296e72d022f1a36122f62c71eeb1f
Martin Hilscher <mh@xinitrc.de>
2025-06-01 15:15:58 +0200

    chore: update dependencies and remove deprecated packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import stylistic from '@stylistic/eslint-plugin-ts';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config(
     {
